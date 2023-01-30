916d121bafebdb8f2c823829b42c701b5344a63d
Ashutosh Uniyal <ashutoshuniyal012@gmail.com>
2023-01-30 09:42:06 +0530

    server address added

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 export const fetchResponse = async (chat) => {
     try{
-        const response = await fetch('http://localhost:3000/',{
+        const response = await fetch('https://chatgpt-aw17.onrender.com/',{
             method:'POST',
             headers:{
                 'Content-Type':'application/json'
